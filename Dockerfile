1ce8b1bef6bfdbe849f58d93bf8d0c0eced47903
Paul Seward-Prior <paul@paulseward.com>
2021-10-12 12:26:15 +0000

    minimal support for writes

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM golang:alpine AS build
 WORKDIR /build
 COPY . .
 RUN go mod download
-RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o tftp
+RUN CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -o tftp
 
 FROM scratch
 COPY --from=build /build/tftp tftp
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"io"
+	"io/ioutil"
 )
 
 func main() {
@@ -12,14 +13,17 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	readHandler := tftp.ReadHandlerFunc(proxyTFTP)
+	readHandler := tftp.ReadHandlerFunc(getFile)
 	s.ReadHandler(readHandler)
+
+	writeHandler := tftp.WriteHandlerFunc(putFile)
+	s.WriteHandler(writeHandler)
 	s. ListenAndServe()
 	select{}
 
 }
 
-func proxyTFTP(w tftp.ReadRequest) {
+func getFile(w tftp.ReadRequest) {
 	log.Printf("[%s] GET %s\n", w.Addr().IP.String(), w.Name() )
 	file, err := os.Open("/tftpboot/" + w.Name()) // For read access.
 	if err != nil {
@@ -32,4 +36,41 @@ func proxyTFTP(w tftp.ReadRequest) {
 	if _, err := io.Copy(w, file); err != nil {
 		log.Println(err)
 	}
+}
+
+func putFile(w tftp.WriteRequest) {
+	log.Printf("write request for %s", w.Name())
+
+	// Get the file size
+	size, err := w.Size()
+	log.Printf("file size from client %d", size)
+
+	// check size of uploaded file is smaller than we're willing to store (32MB)
+	// An error indicates no size was received.
+	//if err != nil || size > 32*1024*1024 {
+		// Send a "disk full" error.
+		//w.WriteError(tftp.ErrCodeDiskFull, "File too large or no size sent")
+		//return
+	//}
+
+	// Note: The size value is sent by the client, the client could send more data than
+	// it indicated in the size option. To be safe we'd want to allocate a buffer
+	// with the size we're expecting and use w.Read(buf) rather than ioutil.ReadAll.
+
+	// Read the data from the client into memory
+	data, err := ioutil.ReadAll(w)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	err = os.WriteFile("/tftpboot/" + w.Name(), data, 0444)
+	if err != nil {
+		log.Println(err)
+		w.WriteError(tftp.ErrCodeDiskFull, err.Error())
+		return
+	}
+
+	// Log a message with the details
+	log.Printf("[%s] PUT %s %d bytes\n", w.Addr().IP.String(), w.Name(), len(data) )
 }
